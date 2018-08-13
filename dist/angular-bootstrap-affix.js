1574347eb02ea13f08615f663ee73b126898079d
Myong Choo <mchoo@enrollment.org>
2018-08-13 11:59:55 -0400

    POR-6594 Support defining an element to use as an offset and set top.

--- dist/angular-bootstrap-affix.js
+++ dist/angular-bootstrap-affix.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * Firefox document.body.scrollHeight = 0; 
  * Because of that the affix-bottom class is constantly added and removed causing flicker.
@@ -8,30 +7,35 @@
  */
 function getDocHeight() {
   var D = document;
-  return Math.max(
-      D.body.scrollHeight, D.documentElement.scrollHeight,
-      D.body.offsetHeight, D.documentElement.offsetHeight,
-      D.body.clientHeight, D.documentElement.clientHeight
-  );
+  return Math.max(D.body.scrollHeight, D.documentElement.scrollHeight, D.body.offsetHeight, D.documentElement.offsetHeight, D.body.clientHeight, D.documentElement.clientHeight);
 }
-
 angular.module('mgcrea.bootstrap.affix', ['mgcrea.jquery']).directive('bsAffix', [
   '$window',
   'dimensions',
   function ($window, dimensions) {
     var checkPosition = function (instance, el, options) {
-
       var scrollTop = window.pageYOffset;
       var windowHeight = window.innerHeight;
-      var scrollHeight = getDocHeight();///document.body.scrollHeight;
+      var scrollHeight = getDocHeight();
+      //document.body.scrollHeight;
       var position = dimensions.offset.call(el[0]);
       var height = dimensions.height.call(el[0]);
       var offsetTop = options.offsetTop * 1;
       var offsetBottom = options.offsetBottom * 1;
+      var top = options.top * 1;
       var reset = 'affix affix-top affix-bottom';
       var affix;
+      if (options.offSetElement) {
+        var offsetElement = getElement(options.offSetElement);
+        if (offsetElement && offsetElement[0]) {
+          top = dimensions.height.call(offsetElement[0]);
+        }
+      }
       if (instance.originTop == null) {
         instance.originTop = position.top;
+        if (top) {
+          instance.originTop = instance.originTop - top;
+        }
       }
       if (windowHeight >= height && instance.originTop <= scrollTop) {
         affix = 'top';
@@ -44,6 +48,13 @@ angular.module('mgcrea.bootstrap.affix', ['mgcrea.jquery']).directive('bsAffix',
         return;
       instance.affixed = affix;
       el.removeClass(reset).addClass('' + (affix ? 'affix affix-' + affix : ''));
+      if (affix) {
+        if (angular.isDefined(top)) {
+          el.attr('style', 'top:' + top + 'px');
+        }
+      } else {
+        el.removeAttr('style');
+      }
     };
     var checkCallbacks = function (scope, instance, iElement, iAttrs) {
       if (instance.affixed) {
@@ -54,11 +65,15 @@ angular.module('mgcrea.bootstrap.affix', ['mgcrea.jquery']).directive('bsAffix',
           eval('scope.' + iAttrs.onAffix);
       }
     };
+    var getElement = function (elementId) {
+      var element = angular.element(document.getElementById(elementId));
+      console.log('element', element);
+      return element;
+    };
     return {
       restrict: 'EAC',
       link: function postLink(scope, iElement, iAttrs) {
         var instance = { unpin: null };
-        //The binding element must be "scrollable"
         angular.element($window).bind('scroll', function () {
           checkPosition(instance, iElement, iAttrs);
           checkCallbacks(scope, instance, iElement, iAttrs);
--- src/bootstrap-affix.js
+++ src/bootstrap-affix.js
@@ -1,21 +1,49 @@
 "use strict";
 
+/**
+ * Firefox document.body.scrollHeight = 0; 
+ * Because of that the affix-bottom class is constantly added and removed causing flicker.
+ * This function fixes that.
+ * Stolen from here: http://james.padolsey.com/snippets/get-document-height-cross-browser/
+ */
+function getDocHeight() {
+  var D = document;
+  return Math.max(
+    D.body.scrollHeight, D.documentElement.scrollHeight,
+    D.body.offsetHeight, D.documentElement.offsetHeight,
+    D.body.clientHeight, D.documentElement.clientHeight
+  );
+}
+
 angular
   .module("mgcrea.bootstrap.affix", ["mgcrea.jquery"])
   .directive("bsAffix", function($window, dimensions) {
     var checkPosition = function(instance, el, options) {
       var scrollTop = window.pageYOffset;
       var windowHeight = window.innerHeight;
-      var scrollHeight = document.body.scrollHeight;
+      var scrollHeight = getDocHeight(); //document.body.scrollHeight;
       var position = dimensions.offset.call(el[0]);
       var height = dimensions.height.call(el[0]);
       var offsetTop = options.offsetTop * 1;
       var offsetBottom = options.offsetBottom * 1;
+      var top = options.top * 1;
       var reset = "affix affix-top affix-bottom";
       var affix;
+
+      if (options.offSetElement) {
+        var offsetElement = getElement(options.offSetElement);
+        if (offsetElement && offsetElement[0]) {
+          top = dimensions.height.call(offsetElement[0]);
+        }
+      }
+
       if (instance.originTop == null) {
         instance.originTop = position.top;
+        if (top) {
+          instance.originTop = instance.originTop - top;
+        }
       }
+
       if (windowHeight >= height && instance.originTop <= scrollTop) {
         affix = "top";
       } else if (windowHeight <= height && scrollTop >= instance.originTop) {
@@ -29,6 +57,13 @@ angular
       el
         .removeClass(reset)
         .addClass("" + (affix ? "affix affix-" + affix : ""));
+      if (affix) {
+        if (angular.isDefined(top)) {
+          el.attr('style', 'top:' + top + 'px');
+        }
+      } else {
+        el.removeAttr('style');
+      }
     };
 
     var checkCallbacks = function(scope, instance, iElement, iAttrs) {
@@ -37,6 +72,12 @@ angular
       } else {
         if (iAttrs.onAffix) eval("scope." + iAttrs.onAffix);
       }
+    };
+
+    var getElement = function (elementId) {
+      var element = angular.element(document.getElementById(elementId));
+      console.log('element', element);
+      return element;
     };
 
     return {
